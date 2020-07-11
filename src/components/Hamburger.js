b87eedd21c4353bccdd34fc47d7c97b07a75baf2
Will Bashelor <wbashelor@gmail.com>
2020-07-11 10:47:16 -0400

    Create overlay menu

---
+++ src/components/Hamburger.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Hamburger = () => <div>Hamburger</div>;
+
+export default Hamburger;
---
+++ src/components/layout.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Header from './Header';
+import OverlayMenu from './OverlayMenu';
+import Hamburger from './Hamburger';
+import Footer from './Footer';
+
+import './layout.css';
+import 'bootstrap/dist/css/bootstrap-grid';
+
+const Primary = styled.main`
+  padding: 110px 0 0 0;
+`;
+
+const Layout = ({ children }) => {
+  const [menuOpen, setmenuOpen] = useState(false);
+
+  const handleOverlayMenu = () => {
+    setmenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <Hamburger />
+      <OverlayMenu />
+      <Header />
+      <Primary id="primary" className="content-area">
+        <main id="main" className="site-main" role="main">
+          {children}
+        </main>
+      </Primary>
+    </>
+  );
+};
+
+Layout.propTypes = { children: PropTypes.node.isRequired };
+
+export default Layout;
