d8aef886fc45a7c2821f69b737af4786566eed18
Will Bashelor <wbashelor@gmail.com>
2020-07-11 11:18:08 -0400

    Overlay Menu

--- src/components/OverlayMenu.js
+++ src/components/OverlayMenu.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
-const OverlayMenu = () => <div>OverlayMenu</div>;
+import WhiteLogo from '../images/tango_logo_white.svg';
+import CloseButton from '../images/tango_close_button.svg';
+
+import { Overlay } from './styles/OverlayMenuStyles';
+const OverlayMenu = () => {
+  return <div>overlay</div>;
+};
 
 export default OverlayMenu;
